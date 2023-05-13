b034af25e8c39efa7ee90f28ca366d97d2bc875b
Bhanu-Ganga-DevOPs <gangabhavanikatraprthi@gmail.com>
2023-05-13 15:56:37 -0400

    testing

--- ex1/sample.tf
+++ ex1/sample.tf
@@ -1,5 +1,8 @@
+
+# Terraform aws_instance creation
+
 resource "aws_instance" "web" {
-  ami           = ami-0b5a2b5b8f2be4ec2
+  ami           = "ami-0b5a2b5b8f2be4ec2"
   instance_type = "t3.micro"
 
   tags = {
