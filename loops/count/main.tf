20c10859249779e4975a2a2ce8eaabfbc6d4e2d1
Bhanu-Ganga-DevOPs <gangabhavanikatraprthi@gmail.com>
2023-05-15 18:36:24 -0400

    testing

--- loops/count/main.tf
+++ loops/count/main.tf
@@ -1,3 +1,21 @@
-resource "null_resource" "dummy" {
 
+#Creates a null resource for 10 times
+resource "null_resource" "null" {
+  count = 10
 }
+
+variable "colors" {
+  default = ["Red","Blue","White","Black"]
+}
+
+resource "null_resource" "null" {
+  count = 10
+
+  provisioner "local-exec" {
+    command = "echo My fav color is - ${var.colors[count.index]}"
+  }
+}
+
+
+
+
